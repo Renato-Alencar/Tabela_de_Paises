9c8ef05a28b161fabbf74747c55e9e83e34e79b3
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-05-18 19:39:57 -0300

    Importa a requisição de API e cria a store com a state countries, mutations SET_DATA e REVERSE_TABLE e actions async listData.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getData } from '@/api/config'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    countries: []
   },
   mutations: {
+    SET_DATA(state, payload) {
+      state.countries = payload
+    },
+    REVERSE_TABLE(state) {
+      state.countries = state.countries.reverse()
+    }
   },
   actions: {
+    async listData({commit}) {
+      const response = await getData()
+      commit('SET_DATA', response.data)
+    }
   },
-  modules: {
-  }
 })
