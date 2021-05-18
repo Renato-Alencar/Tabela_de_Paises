c1c5c40af95b43e8329e78212e248f30b5124a9c
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-05-18 19:43:05 -0300

    Cria o component row com os nomes, capitais e bandeiras dos países.

---
+++ src/components/TableRow.vue
@@ -0,0 +1,28 @@
+<template>
+  <tr class="row">
+    <td>{{table.name}}</td>
+    <td>{{table.capital}}</td>
+    <td><img :src="table.flag" :alt="`Bandeira do país ${table.name}`"></td>
+  </tr>
+</template>
+
+<script>
+  export default {
+    name: 'row',
+    props: {
+      table: {
+        type: Object,
+        default: () => {},
+      },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  .row {
+    img {
+      width: 7vw;
+      height: auto;
+    }
+  }
+</style>
